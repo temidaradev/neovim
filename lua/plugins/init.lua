e1a6c77ce5b616bd2e87ac5ff71827e52b3eebca
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-08-25 16:19:22 +0300

    proper rust setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,23 +12,77 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+  
   {
-  'mrcjkb/rustaceanvim',
-    version = '^6', -- Recommended
-    lazy = false, -- This plugin is already lazy
+    'mrcjkb/rustaceanvim',
+    version = '^6', 
+    lazy = false,
+    ft = "rust",
+    config = function ()
+      local codelldb_pkg = vim.fn.expand("$MASON/packages/codelldb")
+      local extension_path = codelldb_pkg .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
+
+      local cfg = require('rustaceanvim.config')
+
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end
+  },
+  
+  {
+    'rust-lang/rust.vim',
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
   },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+			local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+		end,
+  },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    'rcarriga/nvim-dap-ui', 
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+			require("dapui").setup()
+		end,
+  },
+
+  {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true
+          },
+        },
+      }
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+    end
+  },
 }
